ca11b2aa46890a7947e30af8a1431f51b54e2626
simanto97 <saimumsimanto97@gmail.com>
2023-08-05 00:02:13 +0600

    code pushed 3 from developer simanto

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hello from Simanto</h1>
+      <h1>MERN Stack Developer</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
