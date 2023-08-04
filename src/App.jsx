d036678515c5ba49c119421d37703cbb728a735c
simanto97 <saimumsimanto97@gmail.com>
2023-08-05 00:34:57 +0600

    h2 h3 created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-        <nav className="navbar">
-    <div className="navbar-logo">
-      Logo
-    </div>
-    <div className="navbar-links">
-      <a className="navbar-link" href="#">Home</a>
-      <a className="navbar-link" href="#">About</a>
-      <a className="navbar-link" href="#">Services</a>
-      <a className="navbar-link" href="#">Contact</a>
-    </div>
-  </nav>
+      <nav className="navbar">
+        <div className="navbar-logo">Logo</div>
+        <div className="navbar-links">
+          <a className="navbar-link" href="#">
+            Home
+          </a>
+          <a className="navbar-link" href="#">
+            About
+          </a>
+          <a className="navbar-link" href="#">
+            Services
+          </a>
+          <a className="navbar-link" href="#">
+            Contact
+          </a>
+        </div>
+      </nav>
+      <h2>Simanto</h2>
+      <h3>Mern stack developer</h3>
     </>
   );
 }
