97c0be80b8b2d503cbed8915ed30b82072b20f6f
moktubat <shadhinreja@gmail.com>
2023-08-04 23:49:44 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <nav className="navbar">
+    <div className="navbar-logo">
+      Logo
+    </div>
+    <div className="navbar-links">
+      <a className="navbar-link" href="#">Home</a>
+      <a className="navbar-link" href="#">About</a>
+      <a className="navbar-link" href="#">Services</a>
+      <a className="navbar-link" href="#">Contact</a>
+    </div>
+  </nav>
     </>
   )
 }
